d3373429350e1a5601bb29edaaae9a2360506518
= <=dohaiyen0306@gmail.com>
2020-05-29 14:40:01 +0700

    yen

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlurasightLogin.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+        public ActionResult QuanLiDangKi()
+        {
+            return View();
+        }
+        public ActionResult DichVuHoTro()
+        {
+            return View();
+        }
+        public ActionResult CanBo()
+        {
+            return View();
+        }
+        public ActionResult ThanhToan()
+        {
+            return View();
+        }
+        public ActionResult GhiChu()
+        {
+            return View();
+        }
+        public ActionResult TaiKhoan()
+        {
+            return View();
+        }
+    }
+}
